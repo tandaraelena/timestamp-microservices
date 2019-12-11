98b1a68a705cac6810bf0bb3032a6e46c252eb17
Elena <tandaraelena@yahoo.com>
2019-12-11 15:27:49 +0000

    add routes

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+
+// accepted date format
+const stringDateFormat = /([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/;
+
+// first API endpoint... 
+app.get("/api/timestamp/", function (req, res) {
+  res.status(200).json({
+    unix: new Date().getTime(),
+    utc: new Date().toUTCString(),
+  })
+});
+
+// second api endpoint
+app.get("/api/timestamp/:date", (req, res) => {
+  const insertedDate = req.params.date;
+
+  if(insertedDate.match(stringDateFormat)){
+    res.status(200).json({
+      "unix": new Date(insertedDate).getTime(),
+      "utc": new Date(insertedDate).toUTCString(),
+    })
+  } else {
+    res.status(500).json({
+      "error": "Invalid Date"
+    })
+  }
+})
+
+module.exports = app;
--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 // where your node app starts
 
 // init project
+const app = require("./app")
 var express = require('express');
-var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -17,14 +17,6 @@ app.use(express.static('public'));
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
-
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
