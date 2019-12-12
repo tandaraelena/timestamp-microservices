58540a49501c8d96e8e0ed2f79792e45a7451a13
Elena <tandaraelena@yahoo.com>
2019-12-12 16:15:43 +0000

    add unix timestamp endpoint and install nodemon

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var app = express();
 // accepted date format
 const stringDateFormat = /([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/;
 
+// accepted unix format
+const unixFormat = /^\d{13}$/;
+
 // first API endpoint... 
 app.get("/api/timestamp/", function (req, res) {
   res.status(200).json({
@@ -20,6 +23,11 @@ app.get("/api/timestamp/:date", (req, res) => {
     res.status(200).json({
       "unix": new Date(insertedDate).getTime(),
       "utc": new Date(insertedDate).toUTCString(),
+    })
+  } else if(insertedDate.match(unixFormat)){
+    res.status(200).json({
+      "unix": +insertedDate,
+      "utc": new Date(+insertedDate).toUTCString(),
     })
   } else {
     res.status(500).json({
--- server.js
+++ server.js
@@ -19,6 +19,6 @@ app.get("/", function (req, res) {
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3010, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
