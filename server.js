724c260955f2a62afba792f5ee923b9ca01285c5
Elena <tandaraelena@yahoo.com>
2019-12-13 16:34:01 +0000

    add favicon

--- server.js
+++ server.js
@@ -4,6 +4,8 @@
 // init project
 const app = require("./app")
 var express = require('express');
+var favicon = require('serve-favicon')
+var path = require('path')
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -13,12 +15,15 @@ app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+// https://microservices-proj.herokuapp.com/favicon.ico
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3010, function () {
+var listener = app.listen(process.env.PORT || 8080, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
